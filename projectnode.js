8ec79449eb37bea2a2d570b66cca08d5de065c10
Dharan <rdharan7596@gmail.com>
2018-06-23 18:06:45 +0530

    My first lovely update with some changes

--- projectnode.js
+++ projectnode.js
@@ -2,7 +2,7 @@
     var express = require("express");
     require('http');
     var app = express();
-
+    const PORT = process.env.PORT || 5000;
 
     app.get('/',  function(req, res) {
 
@@ -15,10 +15,10 @@
     });
 
 
-    var server = app.listen(8080, function () {
+    var server = app.listen(PORT, function () {
 
       var host = server.address().address
       var port = server.address().port
-      console.log("Example app lis  tening at http://%s:%s", host, port)
+      console.log("Example app listening at http://%s:%s", host, port)
 
     })
